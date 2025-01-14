7cb772836c8388770613d3b745ae626511f687ee
nryan <ryan.nedjari@ynov.com>
2025-01-14 23:07:56 +0100

    add working data base creat article and connect user

--- blog123/src/App.jsx
+++ blog123/src/App.jsx
@@ -2,10 +2,12 @@
 import './App.css';
 import {Auth} from "./components/auth";
 import React from 'react';
+import CreateArticle from "./components/CreateArticle.jsx";
 function App() {
     return (
         <div className="App">
             <Auth/>
+            <CreateArticle/>
         </div>
     );
 }
